70a77fc6b85d2bcecc8f030eaf97cdb67fa573d9
wwwelliton <84337929+wwwelliton@users.noreply.github.com>
2021-10-08 17:22:42 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+
+<?php
+
+// Mostra todas as informações, usa o padrão INFO_ALL
+//phpinfo();
+
+// Mostra apenas informações dos módulos.
+// phpinfo(8) mostra um resultado identico.
+//phpinfo(INFO_MODULES);
+
+
+for ($i = 0; $i <9; $i++){
+
+$product = 10 * $i;
+
+echo "The product of 10 * $i is $product <br/>";
+}
+
+
+?>
